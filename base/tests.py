265137fbd93e4ac4d26997b9c3e72e60e1b98718
Haitham Odeh <haithamodehodeh@gmail.com>
2022-07-13 21:26:26 +0300

    add api tests and fix dockerfile

--- base/tests.py
+++ base/tests.py
@@ -1,3 +1,18 @@
+from datetime import timezone
 from django.test import TestCase
 
-# Create your tests here.
+from base.models import Product
+
+
+class ProductTest(TestCase):
+    def create_product(self, title="only a test", body="yes, this only a test"):
+        return Product.objects.create(title=title, body=body, created_at=timezone)
+
+    def test_whatever_creation(self):
+        # w = self.create_product()
+        # self.assertTrue(isinstance(w, Product))
+        # self.assertEqual(w.__unicode__(), w.title)
+        pass
+
+    # def test_whatever(self):
+    # self.assertTrue(True == True)
